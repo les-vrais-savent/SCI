fca21e0e468021f23508aba60d1fe3896a4a7535
altor <arthur.dazemar@etudiant.univ-lille1.fr>
2017-09-19 15:55:28 +0200

    param de ligne de commande

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from  model.SMA import SMA
 from  model.Environment import Environment
+
+import argparse
 # from  model.Agent import Agent
 
 
+ap = argparse.ArgumentParser()
 
-env = Environment(10)
-sma = SMA(10, env)
+ap.add_argument("--size", type=int, default=10,
+                help="(optional) size of the grid")
+ap.add_argument("--nb_agent", type=int, default=10,
+                help="(optional) number of agent")
+ap.add_argument("--nb_round", type=int, default=10,
+                help="(optional) number of round")
+
+args = vars(ap.parse_args())
+
+env = Environment(args["size"])
+sma = SMA(args["nb_agent"], env)
+
+for i in range(args["nb_round"]):
+    sma.run()
+
+
