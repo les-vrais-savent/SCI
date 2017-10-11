89ef7b682759661d04dbc7867aa244c936d36ed8
Jordan <jordan.bouchoucha@ntymail.com>
2017-10-11 11:20:03 +0200

    Fix fish

--- src/model/wator/Fish.py
+++ src/model/wator/Fish.py
@@ -18,7 +18,7 @@ class Fish(Animal):
                                   ' lay egg at position : ' + str(posX) +
                                   ',' + str(posY) + '\n')
         """                   
-        return Fish(self.environment, posX, posY, 3, self.trace_file)
+        return Fish(self.environment, posX, posY, breedTime, self.trace_file)
 
     def getColor(self):
         if self.baby:
